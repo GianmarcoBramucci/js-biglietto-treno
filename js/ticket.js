abad61837781e94c64cefb6eea2757149487cba6
GianmarcoBramucci <giangian797@gmail.com>
2023-07-24 16:49:19 +0200

    add script

---
+++ js/ticket.js
@@ -0,0 +1,28 @@
+let numeroKilometri;
+let età;
+const scontoMinorenni=20;
+const scontoPenzionati=40;
+const prezzoKilometri=0.21;
+let prezzoBiglietto;
+do
+{
+    alert("inserisci il valore a numero")
+    numeroKilometri= prompt("inserisci il numero di kilometri");
+    età= prompt("inserisci la tua età");
+}
+while(isNaN(numeroKilometri)||isNaN(età) )
+prezzoBiglietto=parseFloat(numeroKilometri*prezzoKilometri,2);
+if(età>18||età<65)
+{
+    alert(prezzoBiglietto)
+}
+else if(età>65)
+{
+    prezzoBiglietto=parseFloat((prezzoBiglietto*scontoPenzionati)/100,2);
+    alert("pensionati",prezzoBiglietto)
+}
+else
+{
+    prezzoBiglietto=parseFloat((prezzoBiglietto*scontoMinorenni)/100,2);
+    alert("minorenni",prezzoBiglietto)
+}
